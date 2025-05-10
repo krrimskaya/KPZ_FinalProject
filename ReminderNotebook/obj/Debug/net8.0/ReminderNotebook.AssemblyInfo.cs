b0ac48e097a0aeaab1c707f55b890085d4964b5c
Anastasia Vorobey <ipz231_vav@student.ztu.edu.ua>
2025-05-10 17:42:43 +0300

    Add edit reminder feature with time check

--- ReminderNotebook/obj/Debug/net8.0/ReminderNotebook.AssemblyInfo.cs
+++ ReminderNotebook/obj/Debug/net8.0/ReminderNotebook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReminderNotebook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abad18e7ef4bb841c7959d50b1a4a6ee1e07ff99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2850da0b8cf62b17cde8ee54636f67fe6d8b9d86")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReminderNotebook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReminderNotebook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
